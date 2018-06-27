17b7728467552bfc6e9d5547dde89c9dae79f86b
Pieter van Kemenade <pike@speeltuin.kw.nl>
2018-06-27 14:21:34 +0200

    from gra

--- xframe.js
+++ xframe.js
@@ -19,6 +19,7 @@
 	
 */
 
+
 jQuery(document).ready(function() {
 	XFrame.init();
 });
@@ -27,11 +28,19 @@ var XFrame = {
 	
 	config	: {
 		defcontext		: window,
+		defmethod			: 'GET',
 		defsuccess		: function(code,msg) { },
 		deferror			: function(code,msg) { },
 		deftimeout		: 4000,
 		interval			: 0,
-		pauseonerr		: true
+		pauseonerr		: true,
+		defwarn				: 'press <a href="javascript:window.location.reload()">reload</a> to see changes',
+		init					: function() {
+			var $ = jQuery;
+			var $head = $('#table_id_manage_resource thead tr.rowHeaders');
+			var numcols = $head.children().length
+			$head.before('<tr id="xframe-warn"><td colspan="20" id="xframe-warn-content"></td></tr>')
+		}
 	},
 	
 	timer		: null,
@@ -43,27 +52,37 @@ var XFrame = {
 	
 	init	: function() {
 		var $ = jQuery;
-		$('head').append('<style type="text/css" id="xframe-css">'+this.config.css+'</style>');	
-		$('body').append('<div id="xframe-warn">');	
-		$('body').append('<div id="xframe"><header><div id="xframe-close"></div><div id="xframe-queue"></div><div id="xframe-status"></div></header><iframe></iframe></div>');	
+		
+		if (this.config.init) this.config.init();
+		
+		if (!$('#xframe-warn').length) {
+			$('body').append('<div id="xframe-warn"><div id="xframe-warn-content"></div></div>');	
+		}
 		$('#xframe-warn').on('click',function() { XFrame.unwarn(); });
+
+		if (!$('#xframe').length) {
+			$('body').append('<div id="xframe"><header><div id="xframe-close"></div><div id="xframe-queue"></div><div id="xframe-status"></div></header><iframe name="xframe-content"></iframe></div>');	
+		}
 		$('#xframe').on('click',function() { XFrame.dismiss(); });
-		this.$xframe 	= $('#xframe');
-		this.$iframe 	= $('#xframe iframe');
-		this.$status 	= $('#xframe-status');
-		this.$queue 	= $('#xframe-queue');
+		
+		XFrame.$xframe 	= $('#xframe');
+		XFrame.$iframe 	= $('#xframe iframe');
+		XFrame.$status 	= $('#xframe-status');
+		XFrame.$queue 	= $('#xframe-queue');
+		
 	},
 	
-	exec	: function(link,onsuccess,onerror,context,timeout) {
-		this.queue(link,onsuccess,onerror,context,timeout);
+	exec	: function(link,onsuccess,onerror,method,context,timeout) {
+		this.queue(link,onsuccess,onerror,method,context,timeout);
 	},
 	
-	queue	: function(link,onsuccess,onerror,context,timeout) {
+	queue	: function(link,onsuccess,onerror,method,context,timeout) {
 		if (link) {
 			var action = {
 				'link'			: link,
 				'onsuccess'	: onsuccess?onsuccess:this.config.defsuccess,
 				'onerror'		: onerror?onerror:this.config.deferror,
+				'method'		: (method?method:this.config.defmethod).toUpperCase(),
 				'context'		: context?context:this.config.defcontext,
 				'timeout'		: timeout?timeout:this.config.deftimeout
 			};
@@ -80,7 +99,8 @@ var XFrame = {
 			clearTimeout(this.timer);
 			this.current = this.todo.pop();
 			this.$queue.text(this.todo.length?'('+this.todo.length+')':'');
-			this.$iframe.attr('src',this.current.link);
+			if (this.current.method=='GET') this.$iframe.attr('src',this.current.link);
+			else this.post(this.current.link);
 			this.$xframe.removeClass('error success').addClass('pending');	
 			this.$status.text('pending..');
 			XFrame.show();
@@ -90,6 +110,21 @@ var XFrame = {
 		} else {
 			this.current=null;
 		}
+	},
+	
+	post : function(link) {
+		var parts = link.split('?');
+		var url = parts[0];
+		var params = parts[1].split('&');
+		var pp, inputs = '';
+		for(var i = 0, n = params.length; i < n; i++) {
+				pp = params[i].split('=');
+				inputs += '<input type="hidden" name="' + pp[0] + '" value="' + pp[1] + '" />';
+		}
+		if (!$('#xframe-post').length) {
+			$("body").append('<form method="post" id="xframe-post" target="xframe-content">');
+		} 
+		$('#xframe-post').attr('action',url).empty().append(inputs).submit();
 	},
 	
 	success	: function(code,msg) {
@@ -146,10 +181,13 @@ var XFrame = {
 		this.$iframe.attr('src','');
 		this.$xframe.hide();
 	},
+
 	
 	warn			: function(msg,timeout) {
 		var $ = jQuery;
-		$('#xframe-warn').html(msg).show();
+		if (!msg) msg = this.config.defwarn;
+		$('#xframe-warn-content').html(msg);
+		$('#xframe-warn').show();
 		if (timeout) {
 			clearTimeout(this.warntimer);
 			this.warnTimer = setTimeout(function() {
